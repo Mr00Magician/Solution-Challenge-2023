abbfb96abef9e20f754ea9d8cc5d3076a8b0a9c4
Mr00Magician <meanasnadeem@gmail.com>
2023-03-17 16:22:18 +0530

    login_page now redirected to home page if username and password are correct

--- back-end/app.py
+++ back-end/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ value = {
 }
 
 @app.route("/")
-def home_page():
+def login_page():
     return render_template('login.html', value = value)
 
 @app.route('/login', methods = ['POST'])
@@ -22,11 +22,15 @@ def login():
     value['password'] = password = request.form.get('password')
     try:
         if users[username] == password:
-            return render_template('index.html')
+            return redirect(url_for('home_page'))
         else:
             return render_template('login.html', error_message = 'Invalid Password', value = value)
-    except:
+    except Exception as e:
         return render_template('login.html', error_message = 'Invalid Username', value = value)
+    
+@app.route('/home')
+def home_page():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
